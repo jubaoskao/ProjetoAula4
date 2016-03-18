7d65427c6c57eb7b3f6b72baf1dfb5a0e5512e93
145052 <145052@SO02.labinfo.facens.br>
2016-03-18 20:40:02 -0300

    PORRA NENHUMA

--- ProjetoMavenJSFPrimeFaces/src/main/java/sistema/beans/AlunoManagedBean.java
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/beans/AlunoManagedBean.java
@@ -1,10 +1,11 @@
 package sistema.beans;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import org.primefaces.event.SelectEvent;
 
 import sistema.modelos.Aluno;
 import sistema.service.AlunoService;
@@ -16,12 +17,18 @@ public class AlunoManagedBean {
 	private Aluno aluno = new Aluno();
 	private AlunoService service = new AlunoService();
 	
-	
 	public void salvar()
 	{
 		service.salvar(aluno);
 		aluno = new Aluno();
 		
+	}
+	
+	public void remover(Aluno a)
+	{
+		System.out.println("REMOVEP1");
+		service.remover(a);
+		a = new Aluno();
 	}
 
 	public Aluno getAluno() {
@@ -35,9 +42,14 @@ public class AlunoManagedBean {
 	public List<Aluno> getAlunos() {
 		return service.getAlunos();
 	}
-
 	
+	public void onRowSelect(SelectEvent event) {
+	     System.out.println("Evento Select: AlunoSelecionado : " +  (Aluno) event.getObject());
+	}
 	
-	
+	public void onRowUnselect(SelectEvent event) {
+		System.out.println("Evento UnSelect " +  (Aluno) event.getObject());
+			 
+    }
 
 }
--- ProjetoMavenJSFPrimeFaces/src/main/java/sistema/service/AlunoService.java
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/service/AlunoService.java
@@ -30,6 +30,43 @@ public class AlunoService {
 		
 	}
 	
+	public void remover(Aluno aluno){
+		System.out.println("REMOVE2");
+		
+		EntityManager em = emf.createEntityManager();
+		
+		Aluno a1 = em.find(Aluno.class, aluno.getMatricula());
+
+		if (a1 != null) {
+			em.remove(a1);
+			System.out.println("Sucesso");
+		} else {
+			System.out.println("Não cadastrado");
+		}
+	
+		em.getTransaction().begin();
+		em.getTransaction().commit();
+	
+		em.close();
+	}
+	
+	public void editar(Aluno aluno)
+	{
+		EntityManager em = emf.createEntityManager();
+		
+		Aluno l1 = em.find(Aluno.class, aluno.getMatricula());
+		
+		if (l1 != null) {
+			l1.setNome(aluno.getNome());
+			l1.setNome(aluno.getCurso());
+
+			l1 = em.find(Aluno.class, 2l);
+		} else {
+			System.out.println("Não cadastrado");
+		}
+		
+		em.close();
+	}
 	
 	@SuppressWarnings("unchecked")
 	public List <Aluno> getAlunos()
